5c6706f781407bbe2ef64ce566f5ee5b36f0c0ee
BfDmichal <michal.przyborowski@o2.pl>
2019-11-02 21:02:50 +0100

    Ad.23.3

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,22 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Optional;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+    private String mailTo;
+    private String subject;
+    private String message;
+    private String toCc;
+
+    public Mail(String mailTo, String subject, String message) {
+        this.mailTo = mailTo;
+        this.subject = subject;
+        this.message = message;
+        this.toCc = "";
+    }
+}
